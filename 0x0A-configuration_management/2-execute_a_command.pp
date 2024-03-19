520a681955cfaf49ffa6dc6e0d0e1469e80a8be5
Solomon <kyobenimaru001@gmail.com>
2024-03-19 13:42:57 +0300

    commit for the puppet task

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+#  This will kill a process
+
+exec { 'kill_killmenow_process':
+  command     => 'sudo pkill -f killmenow',
+  path        => ['/bin', '/usr/bin'], 
+  refreshonly => true,                  # Only execute when triggered explicitly
+  logoutput   => true,                  # Log the output of the command
+}
