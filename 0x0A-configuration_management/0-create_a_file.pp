6b4b1c2f0458a0a854e69f7e3cd735564d6acf33
Solomon <kyobenimaru001@gmail.com>
2024-03-19 13:32:20 +0300

    commit for the file creations

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# This file creates a file school in the /tmp dir 
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => "I love Puppet\n",
+}
+
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,3 @@
+# By SOLOMON KANIARU
+
+## This project will house the project files related to the project
